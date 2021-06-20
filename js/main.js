7288a297386c20108dcbfafbdcc67c88f03372e9
Sumant Kumar <skhackerkr@gmail.com>
2021-06-20 11:59:10 -0600

    footer and skills section over

--- js/main.js
+++ js/main.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Blogger", "Designer", "Hacker"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer", "Blogger", "Designer", "Hacker"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -70,3 +70,19 @@ $(document).ready(function(){
         }
     });
 });
+let options = {
+    startAngle: -1.55,
+    size: 150,
+    value: 0.85,
+    fill: {gradient: ['#a445b2', '#fa4299']},
+  }
+  $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+  function(event, progress, stepValue){
+    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+  $(".js .bar").circleProgress({
+    value: 0.70
+  });
+  $(".react .bar").circleProgress({
+    value: 0.60
+  });
